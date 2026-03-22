07db3bff8837902ffaef1bbf84e68d1333bedee3
Eric <eric032807@gmail.com>
2026-03-22 16:28:10 -0400

    added board, displayBoard function

--- Group6_TicTacToe_with_AI/src/Board.java
+++ Group6_TicTacToe_with_AI/src/Board.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class Board {
+    public char[][] board;
+    public Board() {
+        board = new char[3][3];
+        
+        int i = 0;
+        int j = 0;
+
+        for(i=0;i<board.length;i++){
+            for(j=0;j<board[i].length;j++){
+                board[i][j]=' ';
+            }
+        }
+    }
+
+    public void displayBoard(){
+        int i = 0;
+        int j = 0;
+        for(i=0; i< board.length;i++){
+
+            for(j=0; j< board[i].length;j++){
+                System.out.print(board[i][j]);
+                if (j < (board[i].length-1)){
+                    System.out.print(" |")
+                }
+            }
+            System.out.println();
+            if(i<(board.length-1)){
+                System.out.print("---------")
+            }
+        }
+    }
 }
